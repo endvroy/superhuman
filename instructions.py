905a3e5e74404a8c92598d7bc77ebfa62325a7c5
Jiaqi Li <jla470@sfu.ca>
2017-11-24 15:26:51 -0800

    Add LineTuple class for beam search

--- instructions.py
+++ instructions.py
@@ -3,70 +3,86 @@ from state import State
 from enum import Enum
 import copy
 
-def add(st, loc):
-    if st.reg is None:
-        raise ValueError('invalid read on null reg')
-    if 0 < loc > len(st.mem):
+def add(st, copyFrom, loc, copyTo):
+    # if st.reg is None:
+    #     raise ValueError('invalid read on null reg')
+    if 0 < loc > len(st.mem) or loc < 0 or st.mem[loc] == None:
         raise ValueError('invalid mem read at loc: ' + str(loc))
-    return State(None, st.reg+st.mem[loc], copy.deepcopy(st.mem), copy.deepcopy(st.output))
+    if 0 < copyFrom > len(st.mem) or st.mem[copyFrom] == None:
+        raise ValueError('invalid mem read at loc: ' + str(loc))
+    if 0 < copyTo > len(st.mem):
+        raise ValueError('invalid mem read at loc: ' + str(loc))
+    newMem = copy.deepcopy(st.mem)
+    newMem[copyTo] = newMem[copyFrom] + newMem[loc]
+    return State(None, newMem, copy.deepcopy(st.output))
 
-def sub(st, loc):
-    if st.reg is None:
-        raise ValueError('invalid read on null reg')
-    if 0 > loc > len(st.mem):
+def sub(st, copyFrom, loc, copyTo):
+    # if st.reg is None:
+    #     raise ValueError('invalid read on null reg')
+    if 0 < loc > len(st.mem) or loc < 0 or st.mem[loc] == None:
         raise ValueError('invalid mem read at loc: ' + str(loc))
-    return State(None, st.reg-st.mem[loc], copy.deepcopy(st.mem), copy.deepcopy(st.output))
+    if 0 < copyFrom > len(st.mem) or st.mem[copyFrom] == None:
+        raise ValueError('invalid mem read at loc: ' + str(loc))
+    if 0 < copyTo > len(st.mem):
+        raise ValueError('invalid mem read at loc: ' + str(loc))
+    newMem = copy.deepcopy(st.mem)
+    newMem[copyTo] = newMem[copyFrom] - newMem[loc]
+    return State(None, newMem, copy.deepcopy(st.output))
 
-def copyFrom(st, loc):
-    if 0 > loc > len(st.mem):
-        raise ValueError('invalid mem read at loc: ' + str(loc))
+# def copyFrom(st, loc):
+#     if 0 > loc > len(st.mem):
+#         raise ValueError('invalid mem read at loc: ' + str(loc))
+#     newSt = State(st)
+#     newSt.reg = newSt.mem[loc]
+#     return newSt
+
+# def copyTo(st, loc):
+#     if st.reg is None:
+#         raise ValueError('invalid read on null reg')
+#     if 0 > loc > len(st.mem):
+#         raise ValueError('invalid mem write at loc: ' + str(loc))
+#     newSt = State(st)
+#     newSt.mem[loc] = newSt.reg
+#     return newSt
+
+def outbox(st, loc):
+    # if st.reg is None:
+    #     raise ValueError('invalid read on null reg')
+    if 0 < loc > len(st.mem) or st.mem[loc] == None:
+        raise ValueError('invalid mem read at loc: ' + str(loc)) 
     newSt = State(st)
-    newSt.reg = newSt.mem[loc]
+    newSt.output.append(st.mem[loc])
     return newSt
 
-def copyTo(st, loc):
-    if st.reg is None:
-        raise ValueError('invalid read on null reg')
-    if 0 > loc > len(st.mem):
-        raise ValueError('invalid mem write at loc: ' + str(loc))
-    newSt = State(st)
-    newSt.mem[loc] = newSt.reg
-    return newSt
-
-def outbox(st):
-    if st.reg is None:
-        raise ValueError('invalid read on null reg')
-    newSt = State(st)
-    newSt.output.append(st.reg)
-    return newSt
-
-class ArgType(object):
-    def __init__(self, useReg=False, argList=[]):
-        self.useReg = useReg
-        self.argList = argList
-    def __str__(self):
-        return '(' + str(self.useReg) + ' ' + str(self.argList) +')'
-    def __repr__(self):
-        return str(self)
+# class ArgType(object):
+#     def __init__(self, useReg=False, argList=[]):
+#         self.useReg = useReg
+#         self.argList = argList
+#     def __str__(self):
+#         return '(' + str(self.useReg) + ' ' + str(self.argList) +')'
+#     def __repr__(self):
+#         return str(self)
 
 
-instArgsMap = {
-    add : ArgType(True, ['l']),
-    sub : ArgType(True, ['l']),
-    copyFrom : ArgType(False, ['l']),
-    copyTo : ArgType(True, ['l']),
-    outbox : ArgType(True, [])
-}
+# instArgsMap = {
+#     add : ArgType(True, ['l']),
+#     sub : ArgType(True, ['l']),
+#     copyFrom : ArgType(False, ['l']),
+#     copyTo : ArgType(True, ['l']),
+#     outbox : ArgType(True, [])
+# }
 
 
 if __name__ == '__main__':
-    st = State(None, 1, [2, 3, 4], [])
+    st = State(None, [2, 3, 4], [])
     print st
-    st = add(0, st)
+    st = add(st, 0, 2, 1)
     print st
-    st = copyFrom(2, st)
+    st = sub(st, 0, 1, 2)
     print st
-    st = sub(2, st)
+    st = outbox(st, 2)
     print st
-    st = outbox(st)
+    st = outbox(st, 1)
+    print st
+    st = outbox(st, 0)
     print st
---
+++ linetuple.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+from state import State
+from enum import Enum
+import instructions as op
+
+class LineTuple(object):
+    # A LineTuple object represents a line of code
+    # A LineTuple should be like "operator op1 op2 op3"
+    # op1 = CopyFrom, op2 = loc op3 = CopyTo
+    def __init__(self, operator=None, state=None, copyFrom=-1, loc=0, copyTo=-1):
+        if operator==None or state == None:
+            raise ValueError('Empty operator or state')
+        self.operator = operator
+        self.copyFrom = copyFrom
+        self.loc = loc
+        self.copyTo = copyTo
+        # Calculate the new state
+        if operator == "add":
+            self.state = op.add(state, copyFrom, loc, copyTo)
+        elif operator == "sub":
+            self.state = op.sub(state, copyFrom, loc, copyTo)
+        elif operator == "outbox":
+            self.state = op.outbox(state, loc)
+        else:
+            raise ValueError("Invalid operator: " + operator)
+
+    def getState(self):
+        return self.state
+    
+    def __str__(self):
+        # return str(self.reg) + ' ' + str(self.mem) + ' ' + str(self.output)
+        return self.operator + ' ' + str(self.state) + ' ' + str(self.copyFrom) + ' ' + str(self.loc) + ' ' + str(self.copyTo)
+
+    def __repr__(self):
+        return str(self)
+
+if __name__ == "__main__":
+    st = State(None, [2,3,4], [])
+    print st
+    line1 = LineTuple("add", st, 0, 2, 1)
+    st = line1.getState()
+    print st
+    line2 = LineTuple("sub", st, 0, 1, 2)
+    st = line2.getState()
+    print st
+    line3 = LineTuple("outbox", st, loc=2)
+    st = line3.getState()
+    print st
+    line4 = LineTuple("outbox", st, loc=1)
+    st = line4.getState() 
+    print st 
+    line5 = LineTuple("outbox", st, loc=0)
+    st = line5.getState()
+    print st
