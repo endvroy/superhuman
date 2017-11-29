cf8ac453552772b95548d92d195872cba71045ad
Ruoyi Wang <endvroy@gmail.com>
2017-11-29 13:49:18 -0800

    verifier completed and tested

--- verifier.py
+++ verifier.py
@@ -1,41 +1,36 @@
 from functools import partial
 
 
-def verify(instrs, initial_state, criteria):
+def verify(linetuples, initial_state, criteria):
     """apply the instructions to the start state
      and check if the result state can pass the criteria
      criteria should be a function with the following signature:
      criteria(end_state) -> True if passed, False otherwise"""
     state = initial_state
-    for instr in instrs:
-        state = instr(state)  # convert to partial functions with the adapter
+    for linetuple in linetuples:
+        opcode, args = linetuple.getInstWithArgs()
+        state = opcode(state, *args)  # convert to partial functions with the adapter
 
     return criteria(state)
 
 
-# todo: test the adapter
-def instrs_adapter(linetuples):
-    return (partial(x.operator,
-                    copyFrom=x.copyFrom,
-                    loc=x.loc,
-                    copyTo=x.copyTo)
-            for x in linetuples)
-
-
 if __name__ == '__main__':
     from linetuple import LineTuple
-    from instructions import add, sub, outbox
     from state import State
 
     init_state = State([7, 42, 1337], [44])
-    linetuples = [LineTuple(add, 'spam', 1, 2, 3),
-                  LineTuple(outbox, 'spam', 2),
-                  LineTuple(sub, 'spam', 3, 1, 1)]
+    linetuples = [LineTuple('add', init_state, 0, 1, 1),
+                  LineTuple('outbox', init_state, 1),
+                  LineTuple('sub', init_state, 2, 0, 0)]
+
+
+    # print(init_state)
 
 
     def criteria(final_state: State):
-        return final_state.mem == [42 + 1337, 42, 7 + 42 + 1337] \
-               and final_state.output == [42]
+        # print(final_state)
+        return final_state.mem == [1330, 49, 1337] \
+               and final_state.output == [44, 49]
 
 
-    print(verify(instrs_adapter(linetuples), init_state, criteria))
+    print(verify(linetuples, init_state, criteria))
